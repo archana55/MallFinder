ca60eb1cb9573f572945612377bc214b0c156fe3
Archana <john@doe.org>
2019-02-02 18:03:23 -0600

    created Hello World

---
+++ src/main/java/org/launchcode/mallfinder/MallFinderApplication.java
@@ -0,0 +1,16 @@
+package org.launchcode.mallfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class MallFinderApplication {
+
+	public static void main(String[] args) {
+		run(MallFinderApplication.class, args);
+	}
+
+}
+
